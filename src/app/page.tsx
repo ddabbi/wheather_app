ced41fdebb97364fc552ea6e3cb3694ac43cf139
ddabbi <suminkk04@gmail.com>
2024-07-07 12:32:12 +0900

    0707

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Navbar from "@/components/navbar/navbar";
+
+export default function Home() {
+  return (
+    <div className={styles.homeContainer}>
+      홈페이지
+      <Navbar/>
+    </div>
+  );
+}
+
+
+// favicon.ico 삭제 -> icon.png (app폴더)
